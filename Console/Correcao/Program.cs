85672da822ac67b89f2cef64766ebdfb307d2998
Thiago Rocha do Nascimento <thiago.r.nascimento@hotmail.com>
2020-11-18 16:48:55 -0300

    Exercicios de Repetição

---
+++ Console/Correcao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Correcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            
+            do {
+                Console.Write("Digite uma nota entre 0 e 10: ");
+                nota = int.Parse(Console.ReadLine());                
+
+            } while(!(nota >= 0 && nota <= 10));   
+            // } while(nota < 0 && nota > 10));   
+
+            Console.WriteLine("Número Aceito");
+        }
+    }   
+}
+
+
